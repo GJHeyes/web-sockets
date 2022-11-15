18d6861e87a82538f62250e7026183b28d17cf4a
AndyBoltonMV <andrew.bolton@multiverse.io>
2022-11-15 11:06:46 +0000

    ref

--- public/script.js
+++ public/script.js
@@ -1,17 +1,25 @@
+// io() is a method from the socket library added in our html file
 const socket = io(),
   form = document.querySelector("form"),
   input = document.querySelector("input"),
   messages = document.querySelector("ul");
 
+// On submission of our form our anonymous callback function will run
 form.addEventListener("submit", function (e) {
+  // event.preventDefault() prevents the defauly action of this particular event, which is to refresh the page
   e.preventDefault();
+  // Condition checking for text written in the input element
   if (input.value) {
+    // Using the "emit" method to send a "chat message" event to our web server with the contents of our input tag
     socket.emit("chat message", input.value);
+    // Resetting the input to empty
     input.value = "";
   }
 });
 
+// Using the "on event" method, on receipt of a chat message event from our web server it will run our callback function
 socket.on("chat message", function (msg) {
+  // This logic inserts the recieved message in a newly created list item node and scrolls to the most recent message
   const item = document.createElement("li");
   item.textContent = msg;
   messages.appendChild(item);
--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
-const express = require("express"),
-  http = require("http"),
-  { Server } = require("socket.io"),
-  path = require("path"),
-  app = express(),
-  server = http.createServer(app),
-  io = new Server(server),
-  html = path.join(__dirname, "..", "public");
+const express = require("express"), // Importing express to create our manipulatable web server
+  http = require("http"), // Importing Node's http module to create a seperate server to listen with
+  { Server } = require("socket.io"), // Importing the Server constructor from socket.io to create our web socket methods
+  app = express(), // Creating our express web server for adding endpoints
+  server = http.createServer(app), // Creating our Node server for use with socket.io
+  io = new Server(server); // Creating our web socket server with our node web server
 
-app.use("/", express.static(html));
+// Adding our public folder to our express server
+app.use("/", express.static("public"));
 
+// Using the "on event" method to run our controller function on connection
 io.on("connection", (socket) => {
+  // Socket is what is recieved when a connection is made, it contains values and methods
+  // Using the "on event" method from our socket to run our callback function when a message has been sent
   socket.on("chat message", (msg) => {
+    // Using the "emit" method to send a chat message to all connected instances
     io.emit("chat message", msg);
   });
 });
 
+// Listening on port 5001
 server.listen(5001);
